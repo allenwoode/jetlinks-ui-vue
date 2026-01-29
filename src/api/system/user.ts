b788a51e2f43554e7ecce49c8a308768972d959e
allenwu <allenwogpt@gmail.com>
2026-01-29 17:30:57 +0800

    release v2.13

--- src/api/system/user.ts
+++ src/api/system/user.ts
@@ -34,7 +34,7 @@ export const queryUserListNoPaging = () => request.post(`/user/_query/no-paging`
 //获取角色列表
 export const getRoleList = (data:any) => request.post('/role/group/detail/_query/tree',data)
 // 获取组织列表
-export const getDepartmentList_api = (data:any) => request.post(`/organization/_all/tree`,data);
+export const getDepartmentList_api = (data:any) => request.post(`/organization/all/tree`,data);
 //
 // // 获取用户信息
 // export const getUser_api = (id: string) => request.get(`/user/detail/${id}`);
